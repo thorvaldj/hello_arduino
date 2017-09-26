ba3dbbd93591e7b5bc64e18366e2940bf0b56cd2
Thorvald Johannessen <thorvaldj@users.noreply.github.com>
2017-09-26 12:47:01 +0200

    Create 04_hello_back.ino

---
+++ 04_hello_back.ino
@@ -0,0 +1,24 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+  String input = readSerial();
+
+  if(input == "hello arduino"){
+    Serial.println("hello user");  
+    }
+  
+  delay(1000);
+} 
+
+
+String readSerial(){
+    String serial_in="";
+  
+  if(Serial.available()>0){
+    serial_in = Serial.readStringUntil('\\');
+  }
+  return serial_in;
+}
